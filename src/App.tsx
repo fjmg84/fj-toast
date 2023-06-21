ea06ae13bcdecb601900ed5ba2408e52bde457fb
fjmg84 <fjmgqba@gmail.com>
2023-06-21 18:17:37 -0400

    remove form

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
-import { FormEvent, useRef } from "react";
+import { useRef } from "react";
 import { ToastPortal } from "./components/ToastPortal";
 import { ToastRefProps } from "./interfaces/ToastInterfaces";
 
 const App = () => {
   const toastRef = useRef<ToastRefProps>();
 
-  // FormEvent<HTMLFormElement>
-  const addToast = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-
-    let { value } = event.target[0];
-
-    if (value)
-      toastRef.current?.addMessage({
-        message: value,
-      });
+  const addToast = (): void => {
+    const message = "hi, my name is Fidel"
+    toastRef.current?.addMessage({
+      message,
+    });
   };
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      <form onSubmit={(event) => addToast(event)}>
-        <input type="text" name="text" />
-        <button>Show Toast</button>
-      </form>
+
+      <button onClick={addToast}>Show Toast</button>
+
       <ToastPortal position={"bottom-left"} ref={toastRef} />
     </div>
   );
