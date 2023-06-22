b8f330796354cb0c85d41bc5a620554fb1a0ddfd
fjmg84 <fjmgqba@gmail.com>
2023-06-21 23:52:03 -0400

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,29 @@ import { ToastRefProps } from "./interfaces/ToastInterfaces";
 
 const App = () => {
   const toastRef = useRef<ToastRefProps>();
+  const messageRef = useRef<HTMLInputElement>(null)
 
-  const addToast = (): void => {
-    const message = "hi, my name is Fidel"
-    toastRef.current?.addMessage({
-      message,
-    });
+  const addToast = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    if (messageRef.current) {
+      toastRef.current?.addMessage({
+        message: messageRef.current.value
+      });
+    }
   };
 
   return (
     <div className="App">
       <h1>Hello</h1>
 
-      <button onClick={addToast}>Show Toast</button>
+      <form onSubmit={addToast}>
+
+        <input ref={messageRef} type="text" name="message" />
+        <button>Show Toast</button>
+
+      </form>
+
 
       <ToastPortal position={"bottom-left"} ref={toastRef} />
     </div>
